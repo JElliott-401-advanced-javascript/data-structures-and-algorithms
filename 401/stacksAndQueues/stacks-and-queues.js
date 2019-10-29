d588bc3761ce6ccf0272d7c7c17d6690b27867a5
pnwjce <elliott.jess@gmail.com>
2019-10-28 21:32:42 -0700

    little tweaks

--- 401/stacksAndQueues/stacks-and-queues.js
+++ 401/stacksAndQueues/stacks-and-queues.js
@@ -1,38 +1,30 @@
 'use strict';
 
 class Node {
-
     constructor (value) {
         this.value = value;
         this.next = null;
     }
-
 }
 
 class Stack {
-
     constructor () {
         this.top = null;
     }
-
     push (value) {
         let newNext = this.top;
         let newTop = new Node(value);
         newNext = this.top;
         this.top.next = newNext;
     }
-
     pop () {
         let popMe = this.top.value;
         this.top = this.top.next;
         return popMe;
     }
-
     peek () {
         return this.top.value;
     }
-
-
 };
 
 class Queue {
@@ -41,7 +33,6 @@ class Queue {
         this.front = null;
         this.rear = null;
     }
-
     enqueue (value) {
         let node = new Node(value);
 
@@ -55,16 +46,13 @@ class Queue {
             this.rear = node;
         }
     }
-
     dequeue () {
         let dequeued = this.front.value;
         this.front = this.front.next;
         return dequeued;
     }
-
     peek () {
         return this.front.value;
     }
 }
-
 modules.exports = { Stack, Queue };
